4ca4a9813d73e31bcf484910f889fa9b8077410b
egorlagunovich <egorlagunovich@gmail.com>
2023-06-12 14:31:22 +0200

    final

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import { useEffect, useState } from "react";
-import CurrencyRow from "./components/CurrencyRow";
 import Footer from "./components/Footer";
+import CurrencyRow from "./components/CurrencyRow";
 
 function App() {
   const [currencyOptions, setCurrencyOptions] = useState([]);
-  const [currentDate, setCurrentDate] = useState();
+  const [currentDate, setCurrentDate] = useState("Time");
+  const [fromCurrency, setFromCurrency] = useState();
+  const [toCurrency, setToCurrency] = useState();
+  const [exchangeRate, setExchangeRate] = useState();
+  const [amount, setAmount] = useState(1);
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true);
+
+  let toAmount, fromAmount;
+  if (amountInFromCurrency) {
+    fromAmount = amount;
+    toAmount = amount * exchangeRate;
+  } else {
+    toAmount = amount;
+    fromAmount = amount / exchangeRate;
+  }
+  const handleFromAmountChange = (e) => {
+    setAmount(e.target.value);
+    setAmountInFromCurrency(true);
+  };
+  const handleToAmountChange = (e) => {
+    setAmount(e.target.value);
+    setAmountInFromCurrency(false);
+  };
   const myHeaders = new Headers();
   myHeaders.append("apikey", "UQ1pMFSPWw4PRBYpVacLVt8EBz1B99aT");
   const requestOptions = {
@@ -19,11 +41,26 @@ function App() {
     )
       .then((response) => response.json())
       .then((result) => {
+        const currency = Object.keys(result.rates)[2];
         setCurrencyOptions([...Object.keys(result.rates)]);
         setCurrentDate(result.date);
+        setFromCurrency(result.base);
+        setToCurrency(currency);
+        setExchangeRate(result.rates[currency]);
       })
       .catch((error) => console.log("error", error));
   }, []);
+  useEffect(() => {
+    fetch(
+      `https://api.apilayer.com/exchangerates_data/latest?symbols=JPY%2C%20GBP%2C%20USD%2C%20BYN%2C%20PLN%2C%20EUR&base=${fromCurrency}`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        setExchangeRate(result.rates[toCurrency]);
+      })
+      .catch((error) => console.log("error", error));
+  }, [fromCurrency, toCurrency]);
   return (
     <div className=" p-10 bg-gray-200 min-h-screen">
       <div className="container max-w-md mx-auto">
@@ -35,8 +72,20 @@ function App() {
             1 usd is 0.9 euro
           </h2>
           <p className=" text-gray-700 mb-5">{currentDate}</p>
-          <CurrencyRow currencyOptions={currencyOptions} />
-          <CurrencyRow currencyOptions={currencyOptions} />
+          <CurrencyRow
+            currencyOptions={currencyOptions}
+            selectedCurrency={fromCurrency}
+            onChangeCurrency={(e) => setFromCurrency(e.target.value)}
+            onChangeAmount={handleFromAmountChange}
+            amount={fromAmount}
+          />
+          <CurrencyRow
+            currencyOptions={currencyOptions}
+            selectedCurrency={toCurrency}
+            onChangeCurrency={(e) => setToCurrency(e.target.value)}
+            onChangeAmount={handleToAmountChange}
+            amount={toAmount}
+          />
         </div>
       </div>
       <Footer />
--- src/components/CurrencyRow.js
+++ src/components/CurrencyRow.js
@@ -1,12 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function CurrencyRow(props) {
-  const [fromCurrency, setFromCurrency] = useState();
-  const [toCurrency, setToCurrency] = useState();
-  const [fromCurrencyNum, setFromCurrencyNum] = useState();
-  const [toCurrencyNum, setToCurrencyNum] = useState();
-
-  const onFromCurrencyNum = () => {};
   return (
     <div className="flex flex-row items-center justify-center mb-7 rounded-xl md:text-lg text-md">
       <div>
@@ -14,7 +8,8 @@ export default function CurrencyRow(props) {
           type="number"
           className="p-2 border-none focus:outline-none  rounded-l-xl"
           min="0"
-          onChange={onFromCurrencyNum}
+          value={props.amount}
+          onChange={props.onChangeAmount}
         />
       </div>
       <div>
@@ -22,6 +17,8 @@ export default function CurrencyRow(props) {
           className="block w-full p-2 dark:bg-gray-700 dark:text-white 
           rounded-r-xl
           focus:outline-none"
+          value={props.selectedCurrency}
+          onChange={props.onChangeCurrency}
         >
           {props.currencyOptions.map((option) => (
             <option key={option} value={option}>
